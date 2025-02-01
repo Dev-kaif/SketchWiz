5d476e47ea0c4ffe649e07ad88e0f6c55f637ccb
Mohammad Kaif <kaifghalib123@gmail.com>
2025-02-02 01:46:50 +0530

    chnages inmport for files

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,9 +2,10 @@ import express, { Request, Response } from 'express';
 import { z } from 'zod';
 import cors from 'cors'
 import bcrypt from 'bcrypt';
+import { CreateRoomSchema,CreateUserSchema,SigninSchema } from '@repo/common/type';
 
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from './config';
+import { JWT_SECRET } from '@repo/backend/config';
 import auth from './auth';
 
 console.log(JWT_SECRET);
@@ -16,21 +17,20 @@ app.use(express.json());
 
 app.use(cors());
 
-const reqbody = z.object({
-    email:z.string().email(),
-    username:z.string().min(1).max(30),
-    password:z.string().min(8, { message: "Password must be at least 8 characters long" })
-})
+type signUpRequest = z.infer<typeof CreateUserSchema>
+type signInRequest = z.infer<typeof SigninSchema>
 
-type signUpRequest = z.infer<typeof reqbody>
+interface RequestWithUserId extends Request{
+    userId?:string;
+}
 
 app.post("/api/signup",async (req:Request,res:Response)=>{
 
-    const validation = reqbody.safeParse(req.body)
+    const validation = CreateUserSchema.safeParse(req.body)
     
     if(!validation.success)return;
     
-    const {email,username,password} = req.body;
+    const {email,username,password}:signUpRequest = req.body;
 
     const hashPassword = await bcrypt.hash(password,saltRounds)
     
@@ -41,17 +41,15 @@ app.post("/api/signup",async (req:Request,res:Response)=>{
 })
 
 app.post("/api/signin",(req:Request,res:Response)=>{
-    const {email,password}:Pick<signUpRequest,'email'|'password'> = req.body;
+    const validation = SigninSchema.safeParse(req.body)
+    
+    if(!validation.success)return;
+
+    const {email,password}:signInRequest = req.body;
 
     res.status(200).json({ message: "Signin successful" });
 
 })
-
-interface RequestWithUserId extends Request{
-    userId?:string;
-}
-
-
 
 app.get("/api/room",auth,(req:RequestWithUserId ,res:Response)=>{
     
