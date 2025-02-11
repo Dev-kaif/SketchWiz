5ee219afa2f18f6b3d89d9b071ab3b008b925fc6
Mohammad Kaif <kaifghalib123@gmail.com>
2025-02-11 13:59:27 +0530

    fixed tailwind issue

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,10 +1,13 @@
-// tailwind config is required for editor support
-
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "content" | "presets"> = {
-  content: ["./app/**/*.tsx","./Component/**/*.tsx"],
+  content: [
+    "./app/**/*.tsx",
+    "./Component/**/*.tsx",
+    "../../packages/ui/src/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [sharedConfig],
 };
 
