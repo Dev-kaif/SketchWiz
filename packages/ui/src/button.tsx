fdd45eb723f862b6d7ddd3e6b3895885b966a428
Mohammad Kaif <kaifghalib123@gmail.com>
2025-02-12 00:38:18 +0530

    updated ui components

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode;
-  className: "primary" | "secondary";
+  className: "primary" | "secondary" | "destructive";
   onClickHandler: ()=>void;
   size?:string;
   processing?:boolean
@@ -13,8 +13,9 @@ interface ButtonProps {
 export const Button = ({ children, className, onClickHandler,size,processing }: ButtonProps) => {
 
   const buttonStyle = {
-    primary:`${processing? "bg-[#4f3e80]":"bg-[#6D54B5] "} py-2 px-5 rounded-md w-full ${size}`,
-    secondary:`bg-[#665BA1] rounded-full px-4 py-1 ${size}`
+    primary:`${processing? "bg-gray-400":"bg-white hover:bg-gray-200"} border border-[#1A73E8] text-[#0D2538] font-bold py-3 px-6 rounded-full  ${size}`,
+    destructive:`${processing? "bg-red-900":"bg-red-500 hover:bg-red-700"} py-2 px-5 rounded-md ${size}`,
+    secondary:`bg-[#665BA1] hover:bg-[#4f477c] rounded-full px-4 py-1 ${size}`
   }
 
   const style = buttonStyle[className] || ""
--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -2,19 +2,24 @@ function Input({
   place,
   onChangeHandle,
   type,
+  size,
+  inputRef
 }: {
   place: string;
   type?: string;
+  size?:string;
   onChangeHandle?: (e: string) => void;
+  inputRef?: React.RefObject<HTMLInputElement | null>
 }) {
   return (
     <div>
       <input
+      ref={inputRef}
         onChange={(e) => {
           if (!onChangeHandle) return;
           onChangeHandle(e.target.value);
         }}
-        className="bg-[#3C364C]  w-full px-5 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#7a61c4] focus:border-transparent "
+        className={`${size} bg-[#0D2538]/80 text-white placeholder-gray-400 border border-gray-600 rounded-md px-4 py-3 focus:outline-none focus:border-[#1A73E8] transition-colors duration-300`}
         placeholder={place}
         type={`${type? type:'text'}`}
       />
