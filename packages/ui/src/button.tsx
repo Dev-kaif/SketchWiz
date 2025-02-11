09e870110158301719035e30307ed89e09e51b6b
Mohammad Kaif <kaifghalib123@gmail.com>
2025-02-11 14:01:38 +0530

    fixed auth ui button issue

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -13,7 +13,7 @@ interface ButtonProps {
 export const Button = ({ children, className, onClickHandler,size,processing }: ButtonProps) => {
 
   const buttonStyle = {
-    primary:`bg-[${processing?"#4f3e80":"#6D54B5"}] py-2 px-5 rounded-md w-full ${size}`,
+    primary:`${processing? "bg-[#4f3e80]":"bg-[#6D54B5] "} py-2 px-5 rounded-md w-full ${size}`,
     secondary:`bg-[#665BA1] rounded-full px-4 py-1 ${size}`
   }
 
