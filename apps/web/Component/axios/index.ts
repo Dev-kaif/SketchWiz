d34ee87e37a397e1f34b0ea1c48f021a1efe8d3c
Mohammad Kaif <kaifghalib123@gmail.com>
2025-02-11 11:57:06 +0530

    made common axios export

---
+++ apps/web/Component/axios/index.ts
@@ -0,0 +1,20 @@
+// src/services/axiosConfig.ts
+
+import axios from 'axios';
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authorization");
+
+    if (token) {
+      config.headers["authorization"] = `${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axios;
